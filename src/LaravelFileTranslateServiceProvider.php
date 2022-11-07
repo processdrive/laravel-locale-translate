a6efbc5f02e6a9646899a9ac22d2d0930668f9bc
Santhosh A <santhosh@processdrive.com>
2022-11-07 12:08:57 +0530

    added the automatically run artisan command

--- src/LaravelFileTranslateServiceProvider.php
+++ src/LaravelFileTranslateServiceProvider.php
@@ -8,13 +8,15 @@ class LaravelFileTranslateServiceProvider extends ServiceProvider
 {
     public function boot() {
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        Artisan::call('migrate');
         $this->commands([
             LaravelFileTranslateDbStoreCommand::class,
             LaravelFileRetriveDbValueCommand::class,
         ]);
         $this->loadViewsFrom(__DIR__.'/views', 'LaravelFileTranslate');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        Artisan::call('migrate');
+        Artisan::call('translate:filetodb');
+        Artisan::call('translate:dbtofile');
     }
 
     public function register() {  
